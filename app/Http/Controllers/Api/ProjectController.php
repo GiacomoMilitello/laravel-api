8ab2c134727e6753d99d164f9f13e882c5d9694e
GiacomoMilitello <g.militello95@gmail.com>
2024-04-11 20:30:32 +0200

    router

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,21 @@ class ProjectController extends Controller
             'projects' => $projects,
         ]);
     }
+
+    public function show( $slug ){
+        $project = Project::with( 'type', 'technologies' )->where( 'slug', $slug)->first();
+
+        if( $project ){
+            return response()->json([
+                'seccess' => true,
+                'project' => $project,
+            ]);
+        } else {
+            return response()->json([
+                'seccess' => false,
+                'error' => 'Impossibile trovare Progetto',
+            ]);
+        }
+        
+    }
 }
